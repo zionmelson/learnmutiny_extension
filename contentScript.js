734bc8ddd81a12f32cf16f781ac53e46bb942510
Zion Melson <zionmelson@gmail.com>
2022-11-28 20:08:37 -0500

    default extension set up

--- contentScript.js
+++ contentScript.js
@@ -1,14 +1,24 @@
-() => {
-  let currentPage = "";
+// let currentPage = "";
 
-  chrome.runtime.onMessage.addListener((obj, sender, response) => {
-    const { type, value, pageId } = obj;
+// chrome.runtime.onMessage.addListener((obj, sender, response) => {
+//   const { type, value, pageId } = obj;
 
-    if (type === "NEW") {
-      currentPage = videoId;
-      newPage();
-    }
-  });
+//   if (type === "NEW") {
+//     currentPage = videoId;
+//     newPage();
+//   }
+// });
 
-  const newPage = () => {};
+async function getCurrentTab() {
+  let queryOptions = { active: true, lastFocusedWindow: true };
+  // `tab` will either be a `tabs.Tab` instance or `undefined`.
+  let [tab] = await chrome.tabs.query(queryOptions);
+  return tab;
+}
+
+const newPage = () => {
+  console.log("HERE IS TEST");
 };
+
+newPage();
+getCurrentTab();
