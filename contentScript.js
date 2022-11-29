d1717c85cd4de4e33bcbfb7701462ae3581d87a7
Zion Melson <zionmelson@gmail.com>
2022-11-28 20:14:13 -0500

    quick fix

--- contentScript.js
+++ contentScript.js
@@ -1,15 +1,7 @@
 // let currentPage = "";
 
-// chrome.runtime.onMessage.addListener((obj, sender, response) => {
-//   const { type, value, pageId } = obj;
-
 chrome.runtime.onMessage.addListener((obj, sender, response) => {
   const { type, value, pageId } = obj;
-
-  if (type === "NEW") {
-    currentPage = videoId;
-    newPage();
-  }
 });
 
 const newPage = () => {
@@ -17,4 +9,3 @@ const newPage = () => {
 };
 
 newPage();
-getCurrentTab();
